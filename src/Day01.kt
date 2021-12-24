12377a05f9f7ba3682da5ca8f5573e29f9c76d7c
galiagacandia <galiagacandia@gmail.com>
2021-12-24 13:02:23 -0400

    Primer avance

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var aumenta : Int = 0;
+        var valAnterior : Int = -1;
+        var valActual : Int = 0;
+
+        valActual = input.get(0).toInt();
+        input.forEach {
+            if(valAnterior < valActual){
+
+            }
+        }
+
+
+
+
+        input.forEach {
+            valActual = it.toInt();
+            if (valAnterior != -1) {
+
+            }
+            println(it)
+        }
+        return 10;
     }
+
+    /*
+    fun part1(input: List<String>): Int {
+        return input.size
+    }*/
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -9,9 +35,12 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 10)
 
+    /*
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
+     */
 }
